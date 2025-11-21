0c2c40ea5d5b8a739112666d97a853e0d6cade37
goncalvesgilciara-eng <goncalvesgilciara@gmail.com>
2025-11-21 15:56:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ df['Date_reported'] = pd.to_datetime(df['Date_reported'])
 fig1 = px.line(df, x='Date_reported', y='Cumulative_cases', color='Country', title='Casos Acumulados de Covid-19-Ano 2020')
 fig1.update_layout(xaxis_title='Data', yaxis_title='Casos Acumulados')
 fig1.show()
-
 st.plotly_chart(fig1, use_container_width=True)
+
+df_brasil_usa_india = df.query('Country in ["Brazil", "India", "United States of America"]')
+fig2 = px.line(df_brasil_usa_india, x='Date_reported', y='Cumulative_cases', color='Country', title='Casos Acumulados de Covid-19-Ano 2020')
+fig2.update_layout(xaxis_title='Data', yaxis_title='Brasil x USA x India')
+fig2.show()
+st.plotly_chart(fig2, use_container_width=True)
+
+df_brasil_usa_india = df.query('Country in ["Brazil", "India", "United States of America"]')
+fig3 = px.pie(df_brasil_usa_india, values='Cumulative_cases', names='Country', title='Casos Acumulados de Covid-19-Ano 2020')
+fig3.show()
+st.plotly_chart(fig3, use_container_width=True)
